9b86898aada10416541aa07e8e491f42c7f9dae1
pgaticac <pamela.gatica@usm.cl>
2022-05-09 12:50:05 -0400

    Programación Orientada a Objetos

---
+++ Semana10/Datos/Persona.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Datos
+{
+    public class Persona
+    {
+        private string _nombre;
+        private string _apellido;
+        private DateTime _fechaNacimiento;
+        
+
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get => _fechaNacimiento; set => _fechaNacimiento = value; }
+
+        public int Edad
+        {
+            get
+            {
+                return this.CalcularEdad();
+            }
+        }
+
+        public Persona()
+        {
+
+        }
+
+        public Persona(string nombre, string apellido)
+        {
+            this._nombre = nombre;  //asigno directamente en el campo. En java: this.nombre = nombre;
+            this.Apellido = apellido; //asigno mediante su propiedad. En java: this.setApellido(apellido);
+        }
+
+        private int CalcularEdad()
+        {
+            int añoActual = DateTime.Today.Year;
+            int añoNacimiento = this.FechaNacimiento.Year;
+
+            return añoActual - añoNacimiento;
+        }
+
+      
+        public override string ToString()
+        {
+            return this.Nombre + " " + this.Apellido + " " + this.Edad + " años";
+        }
+
+
+
+    }
+}
---
+++ Semana10/Presentacion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+
+namespace Presentacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Pedro";  //set
+            persona.Apellido = "Parker"; //set
+            persona.FechaNacimiento = new DateTime(2000, 5, 14);
+
+            Persona otraPersona = new Persona("Antonio", "Stark");
+            otraPersona.FechaNacimiento = new DateTime(1980, 9, 24);
+
+          
+
+            Console.WriteLine("Persona: " + persona.Nombre + " " + persona.Apellido + " " + persona.Edad + " años"); //get
+            Console.WriteLine(otraPersona);
+
+            Console.ReadKey();
+        }
+    }
+}
+
